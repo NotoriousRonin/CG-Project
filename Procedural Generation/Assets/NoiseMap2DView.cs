b7ac8d620d5339201c76e28b5807b056ff81f67e
Tony Nguyen <Nguyen.Tony@gmx.de>
2018-12-31 11:27:08 +0100

    Changed Programlayout to MVC, added UI

---
+++ Procedural Generation/Assets/NoiseMap2DView.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseMap2DView : MonoBehaviour {
+
+    /// <summary>
+    /// The Renderer of the Plane
+    /// </summary>
+    public new Renderer renderer;    
+
+    /// <summary>
+    /// Generates a Texture based on the HeightMap given 
+    /// </summary>
+    /// <param name="heightmap">The Heightmap</param>
+    /// <returns>A Texture with each Pixel having a Color depending on their Height</returns>
+    private static Texture2D GenerateTexture(float[,] heightmap)
+    {
+        Texture2D texture = new Texture2D(heightmap.GetLength(0), heightmap.GetLength(1));
+        for (int x = 0; x < heightmap.GetLength(0); x++)
+        {
+            for (int y = 0; y < heightmap.GetLength(1); y++)
+            {
+                float height = heightmap[x, y];
+                Color color = new Color(height, height, height);
+                texture.SetPixel(x, y, color);
+            }
+        }
+        texture.Apply();
+        return texture;
+    }
+
+    public void DrawTexture(float[,] heightmap)
+    {
+        renderer.material.mainTexture = GenerateTexture(heightmap);
+        //renderer.transform.localScale = new Vector3(heightmap.GetLength(0), 1, heightmap.GetLength(1));
+    }
+}
---
+++ Procedural Generation/Assets/NoiseMapController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseMapController : MonoBehaviour {
+
+    /// <summary>
+    /// The Width of the View to generate a Map for
+    /// </summary>
+    public int mapWidth = 256;
+
+    /// <summary>
+    /// The Length of the View to generate a Map for
+    /// </summary>
+    public int mapLength = 256;
+
+    /// <summary>
+    /// Max Height for a Mountain
+    /// </summary>
+    public int mountainHeight = 30;
+
+    /// <summary>
+    /// Frequency of Noise
+    /// </summary>
+    public float noiseFrequency = 8f;
+
+    /// <summary>
+    /// Number of Octaves to be added
+    /// </summary>
+    public int octaveCount = 1;
+
+    /// <summary>
+    /// Controls Decrease in Amplitude of Octaves
+    /// </summary>
+    public float persistance = 0.5f;
+
+    /// <summary>
+    /// Controls Increase in Frequency of Octaves
+    /// </summary>
+    public float lacunarity = 2;
+
+    /// <summary>
+    /// Perlin Noise gives the same Values, to create Randomness => Seed
+    /// </summary>
+    public int seed;
+
+    /// <summary>
+    /// Checks if GenerateView() should be run in Update()
+    /// </summary>
+    public bool autoUpdate = false;
+
+    /// <summary>
+    /// Permanent Run
+    /// </summary>
+    public void Update()
+    {
+        if (autoUpdate) GenerateView();
+    }
+
+    /// <summary>
+    /// Generates the View using NoiseMap2DView given Renderer 
+    /// </summary>
+    public void GenerateView()
+    {
+        float[,] heightMap = NoiseMapData.GeneratePerlinHeightMap(mapWidth, mapLength, noiseFrequency, octaveCount, persistance, lacunarity, seed);
+
+        NoiseMap2DView view = GetComponent<NoiseMap2DView>();
+        view.DrawTexture(heightMap);
+    }
+
+    //Setting Methods for UI Controls
+    public void setMountainHeight(float newMountainHeight) 
+    {
+        mountainHeight = (int)newMountainHeight;
+    }
+
+    public void setNoiseFrequency(float newNoiseFrequency) 
+    {
+        noiseFrequency = newNoiseFrequency;
+    }
+
+    public void setOctaveCount(float newOctaveCount) 
+    {
+        octaveCount = (int)newOctaveCount;
+    }
+    
+    public void setPersistance(float newPersistance) 
+    {
+        persistance = newPersistance;
+    }
+    
+    public void setLacunarity(float newLacunarity) 
+    {
+        lacunarity = newLacunarity;
+    }
+
+    public void setSeed(float newSeed)
+    {
+        seed = (int)newSeed;
+    }
+
+    public void setAutoUpdate(bool newAutoUpdate)
+    {
+        autoUpdate = newAutoUpdate;
+    }
+}
